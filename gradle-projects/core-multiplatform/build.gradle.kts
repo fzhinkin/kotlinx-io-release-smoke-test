7621577797e3ff35d99f842ed18e80f858a22e03
Filipp Zhinkin <filipp.zhinkin@jetbrains.com>
2024-02-24 11:52:33 +0100

    Update build configs to properly test web targets

--- gradle-projects/core-multiplatform/build.gradle.kts
+++ gradle-projects/core-multiplatform/build.gradle.kts
@@ -1,4 +1,11 @@
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension
+import org.jetbrains.kotlin.gradle.targets.js.npm.tasks.KotlinNpmInstallTask
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+}
 
 plugins {
     kotlin("multiplatform")
@@ -13,9 +20,17 @@ kotlin {
     jvm {
         jvmToolchain(8)
     }
-    js(IR)
-    wasmWasi()
-    wasmJs()
+    js(IR) {
+        browser()
+        nodejs()
+    }
+    wasmWasi {
+        nodejs()
+    }
+    wasmJs {
+        nodejs()
+        browser()
+    }
 
     ios()
     watchos()
@@ -41,4 +56,13 @@ kotlin {
             }
         }
     }
+}
+
+rootProject.the<NodeJsRootExtension>().apply {
+    nodeVersion = "21.0.0-v8-canary202310177990572111"
+    nodeDownloadBaseUrl = "https://nodejs.org/download/v8-canary"
+}
+
+rootProject.tasks.withType<KotlinNpmInstallTask>().configureEach {
+    args.add("--ignore-engines")
 }
